7f2eb943dad1045d2415eb77f717a2d7a019ee11
Kwabena Asare <twumgideonasare@gmail.com>
2023-10-03 17:17:26 +0000

    restructure

--- app/components/Cardmusic.jsx
+++ app/components/Cardmusic.jsx
@@ -79,33 +79,30 @@ const Cardmusic = () => {
         {
         songs.map((song)=> (
             <div>        
-                        <div className='flex flex-col m-auto bg-white hover:shadow-xl p-4 hover:cursor-pointer w-[100%] rounded-lg  mt-6'>
+                        <div className='flex flex-col m-auto bg-white hover:shadow-xl p-3  hover:cursor-pointer w-[100%] rounded-xl  mt-6'>
                                 {/* <p className='font-semibold text-black'>#{song.id}</p> */}
                                     <div className='flex items-center justify-between gap-36'>
                                         <div className='flex gap-2 items-center'>
                                                 <div className='ring-1 ring-[#ccc]  rounded-full'>
-                                                    <Image className='rounded-full'
-                                                    width={200}
-                                                    height={200}
+                                                    <Image className='rounded-xl'
+                                                    width={70}
+                                                    height={70}
                                                     src={`https://music-api-qkmn.onrender.com/uploads/${song.photo}`}
                                                     alt=''
 
                                                     />
                                                 </div>
                                                     <div className='flex flex-col '>
-                                                            <p className='font-semibold flex'>  {song.song_name}</p> 
-                                                            <p className='text-sm flex'>  {song.artiste_name}</p>
-                                                            <audio controls>
+                                                            <p className='font-semibold text-[13px] flex'>  {song.song_name}</p> 
+                                                            <p className='text-[12px] flex'>  {song.artiste_name}</p>
+                                                            <audio className='h-[30px] w-[50]' controls>
                                                             <source src={`https://music-api-qkmn.onrender.com/uploads/${song.song}`} type="audio/mpeg" />
                                                             Your browser does not support the audio element.
                                                             </audio> 
-                                                            <p className='text-[12px] flex items-center gap-1'>{song.streams} streams</p> 
-                                                            <p className='text-[12px] flex items-center gap-1'> {likes} likes   <FavoriteBorderIcon className='text-red-400' onClick={() => setLikes(likes + 1)} /> </p>
+                                                            <p className='text-[10px] flex items-center gap-1'>{song.streams} streams</p> 
+                                                            <p className='text-[10px] flex items-center gap-1'> {likes} likes   <FavoriteBorderIcon className='text-red-400' onClick={() => setLikes(likes + 1)} /> </p>
                                                     </div>
                                         </div>
-                                                    <div>
-                                                    <PlayArrowIcon /> 
-                                                    </div>
                                     </div>
                         </div>
             </div>
