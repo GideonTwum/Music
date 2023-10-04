5c57b3e58af37538fa7e2eabb109f8c818a34cfa
Kwabena Asare <twumgideonasare@gmail.com>
2023-10-04 17:01:21 +0000

    responsive

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='mt-10 absolute bottom-0 w-full'>
+    <div className='mt-10 absolute bottom-0 w-full footer'>
         <footer className='bg-[#403e3d] p-[20px]'>
                 <p className='text-[#ccc] text-sm'>copyright</p>
         </footer>
